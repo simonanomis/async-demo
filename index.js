a2428eb1033effa28f55627989dce3a5b7450ab0
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-05-26 19:41:24 +0200

    changed methods with promise

--- index.js
+++ index.js
@@ -14,16 +14,29 @@ function displayCommits(commits) {
     console.log(commits);
 }
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        callback({id: id, gitHubUsername: 'simonanomis'})
-    }, 2000);
+function getUser(id) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({id: id, gitHubUsername: 'simonanomis'})
+        }, 2000);
+    });  
 }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('Calling GitHib API ...');
-        callback(['repo1', 'repo2', 'repo3'])
-    }, 2000);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHib API ...');
+            resolve(['repo1', 'repo2', 'repo3'])
+        }, 2000);
+    });
+}
+
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['commit']);
+        }, 2000);
+    });
 }
