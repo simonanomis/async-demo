fd4a92e59a530cc67c37593a4cdebbce58047555
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-05-26 16:18:30 +0200

    return result with callback

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user from a database...');
-}, 2000);
+getUser(1, function(user) {
+    console.log('User', user);
+    getRepositories(user.gitHubUsername, function(repositories) {
+        console.log('Repositories', repositories);
+    });
+});
 console.log('After');
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        callback({id: id, gitHubUsername: 'simonanomis'})
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHib API ...');
+        callback(['repo1', 'repo2', 'repo3'])
+    }, 2000);
+}
